06b056b7869ac655838d64e96d2dd2dc626a00b8
Sangwon Lee <sang4939@gmail.com>
2024-10-21 10:22:41 +0900

    developing~

---
+++ src/test/java/org/sunbong/board_api1/member/repository/gitTest.java
@@ -0,0 +1,18 @@
+package org.sunbong.board_api1.member.repository;
+
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Log4j2
+public class gitTest {
+
+    @Test
+    public void test() {
+       log.info("안녕하세요");
+       log.info("안녕하세요!!");
+       log.info("안녕하세요");
+    }
+}
