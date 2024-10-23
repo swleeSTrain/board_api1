c0f94d32a224e8d2aeca3946dadbd437f838c67b
Sangwon Lee <sang4939@gmail.com>
2024-10-23 17:33:57 +0900

    안쓰는거 삭제 수정

--- src/main/java/org/sunbong/board_api1/config/WebConfig.java
+++ src/main/java/org/sunbong/board_api1/config/WebConfig.java
@@ -1,11 +1,13 @@
 package org.sunbong.board_api1.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/v1/**")
@@ -13,5 +15,12 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 이미지 파일을 서빙하기 위한 설정 추가
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:///C:/upload/"); // 실제 업로드 경로
     }
 }
