d4e8d98532533f257a6e97cf452e4c4d8028066f
Sangwon Lee <sang4939@gmail.com>
2024-10-25 14:41:45 +0900

    notice out

--- src/main/java/org/sunbong/board_api1/notice/repository/NoticeRepository.java
+++
@@ -1,8 +0,0 @@
-package org.sunbong.board_api1.notice.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.sunbong.board_api1.notice.domain.Notice;
-import org.sunbong.board_api1.notice.repository.search.NoticeSearch;
-
-public interface NoticeRepository extends CrudRepository<Notice, Long>, NoticeSearch {
-}
