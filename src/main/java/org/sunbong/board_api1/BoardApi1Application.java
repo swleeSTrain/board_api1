8b0343434d185d26030a08b4d53a8c8eececce9c
Sangwon Lee <sang4939@gmail.com>
2024-10-25 15:10:26 +0900

    watchtower

--- src/main/java/org/sunbong/board_api1/BoardApi1Application.java
+++ src/main/java/org/sunbong/board_api1/BoardApi1Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BoardApi1Application {
-//집에가자
+
     public static void main(String[] args) {
         SpringApplication.run(BoardApi1Application.class, args);
     }
